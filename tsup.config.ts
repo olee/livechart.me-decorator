65ef8af2dac6f4801fefb5ae0410125e95643d4d
Björn Zeutzheim <dev@bzeutzheim.de>
2024-03-05 04:05:30 +0100

    Basic old version

---
+++ tsup.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'tsup';
+
+const userScriptHeader = `// ==UserScript==
+// @name         Add watch links to livechart.me timetable
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add watch links to livechart.me timetable
+// @author       You
+// @match        https://www.livechart.me/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=livechart.me
+// @grant        none
+// ==/UserScript==`;
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    bundle: true,
+    format: ['iife'],
+    outDir: 'build',
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    minify: false,
+    banner: {
+        js: userScriptHeader,
+    }
+});
